046c38ec03608e50a58ea2e02fd3503100696087
abhi24 <abhishekshinde2211@gmail.com>
2024-01-01 08:00:50 +0530

    Add: Delete functionality using tombstone for marking key as deleted

--- example/main.go
+++ example/main.go
@@ -14,15 +14,24 @@ func main() {
 
 	db.Set("name",[]byte("abhishek"))
 	db.Set("a1",[]byte("A1"))
+	db.Set("a1",[]byte("Z1"))
 	db.Set("b1",[]byte("B1"))
 	db.Set("c1",[]byte("C1"))
 	db.Set("d1",[]byte("D1"))
 
 	value, err := db.Get("a1")
 	if err != nil{
-		panic(err	)
+		panic(err)
 	}
+	fmt.Printf("value for key %s value %s \n","a1",string(value))
 
-	fmt.Println("value for key name ",string(value))
+	err = db.Delete("a1")
+	if err != nil{
+		panic(err)
+	}
+	value, err = db.Get("a1")
+	if value == nil{
+		fmt.Printf("key %s is deleted \n","a1")
+	}
 
 }
